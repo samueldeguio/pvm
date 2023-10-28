1c6c27432283afb8749f1f3c88535a9877256753
samueldeguio <samueldeguio@gmail.com>
2023-10-28 21:45:36 +0200

    add check dependency, command docs and update command

--- app.py
+++ app.py
@@ -1,30 +1,55 @@
 import typer
-import rich
-import os
 
-from include.PHPVersionManager import PHPVersionManager
+from rich.console import Console
 
+from include.PHPVersionManager import PHPVersionManager, PHPVersionManagerException
+from include.ConsoleHelper import ConsoleHelper
+from include.PHP import PHP
+
+# setup main app and console object
 app = typer.Typer()
+console = Console()
+ch = ConsoleHelper(console) 
 
-@app.command()
+@app.command(help="Install the given PHP version")
 def install(version: str = typer.Argument(..., help="PHP version to install to install")):
     pass
 
-@app.command()
+@app.command(help="Set the PHP version to use globally")
 def use (version: str = typer.Argument(..., help="PHP version to use")):
     pass
 
-@app.command()
+@app.command(help="Set the PHP version to use locally on current folder")
 def local(version: str = typer.Argument(..., help="PHP version to use locally on current folder")):
     pass
 
-@app.command()
+@app.command(help="Unistall the given PHP version")
 def remove(version: str = typer.Argument(..., help="PHP version to remove")):
     pass
 
-@app.command()
+@app.command(help="List all installed PHP versions")
 def ls():
-    pass
+    """
+    ls:
+        List all available PHP versions
+    """
+
+@app.command(help="Update PHP repository with latest versions")
+def update():
+    """
+    update:
+        Fetch updates from PHP versions
+    """
+    
 
 if __name__ == "__main__":
-    app()
+    try:
+    
+        # first check if minimum dependencies are installed
+        PHPVersionManager.checkDependencies()
+
+        # run the app
+        app()
+
+    except PHPVersionManagerException as e:
+        ch.printError(e.__str__(), wide=True)
--- include/PHPVersionManager.py
+++ include/PHPVersionManager.py
@@ -1,4 +1,26 @@
+import subprocess
+
 class PHPVersionManager():
-    @staticmethod
-    def checkDependencies() -> bool :
-        pass
+
+    @classmethod
+    def checkDependencies(cls) -> bool :
+        """
+        CheckDependencies:
+            Check if minimum dependencies are installed
+
+        Throws:
+            PHPVersionManagerException: if any dependency is not installed
+
+        Returns:
+            bool: True if all dependencies are installed, False otherwise
+        """
+
+        # check if docker cli is installed
+        try:
+            subprocess.run(["docker", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+            return True
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            raise PHPVersionManagerException("Docker CLI is not installed")
+        
+class PHPVersionManagerException(Exception):
+    pass
